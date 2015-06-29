7770e5b45667a51bcfb3914afc6d64c1f6a976da
Itay Shtekel <shtekel1@gmail.com>
2015-06-29 14:06:49 +0300

    added table and main functions

--- exercises/dom-and-events/js/main.js
+++ exercises/dom-and-events/js/main.js
@@ -1,3 +1,59 @@
 /**
  * Created by itaysh on 6/29/15.
  */
+
+ProductList = (function(){
+    'use strict';
+
+    // mock items
+    var products = [
+        {
+            "id": 1,
+            "name": "tv",
+            "price": 200
+        },
+        {
+            "id": 2,
+            "name": "computer",
+            "price": 100
+        },
+        {
+            "id": 3,
+            "name": "phone",
+            "price": 50
+        },
+        {
+            "id": 4,
+            "name": "slave",
+            "price": 1000
+        },
+        {
+            "id": 5,
+            "name": "shoko",
+            "price": 5
+        }
+    ];
+
+    /*
+     * items {Object}
+     */
+    function init(items){
+        var i,
+            x = items.length,
+            elements = [];
+        for (i = 0; i < x; i++){
+
+        }
+    }
+
+    function handleChange(){
+
+    }
+
+    init(products);
+
+    return {
+      handleChange: handleChange
+    };
+
+})();
